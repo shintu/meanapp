db396e5c8da494f82921a0ddb9d9200112720030
shintu <shintusingh001@gmail.com>
2015-06-19 04:29:45 +0530

    updated code

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -26,7 +26,7 @@ var all = {
 
   // Secret for session, you will want to change this and make it an environment variable
   secrets: {
-    session: 'quick-api-secret'
+    session: 'Betasoft Systems-secret'
   },
 
   // List of user roles
